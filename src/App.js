d8df197cd625bd6989c89c519bd75087f82f7662
papilon <ademirgus@gmail.com>
2023-06-18 21:10:51 -0300

    feat:modified and deleted for components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import PokemonCard from "./components/PokemonCard";
+import { Container, Grid } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Container maxWidth="false">
+        <Grid container>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
