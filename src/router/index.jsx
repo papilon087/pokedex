83230c261a0c69a46e288d2aa39c0b606e42cca6
papilon <ademirgus@gmail.com>
2023-06-27 14:16:49 -0300

    feat:create routers

---
+++ src/router/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />;
+      <Route path="/profile" element={<Home />} />;
+    </Routes>
+  );
+};
