24f73940fb5364668826aa172adeb061ee2e95ab
papilon <ademirgus@gmail.com>
2023-06-26 21:00:57 -0300

    feat:customizing the pokemon types on the card

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -3,13 +3,14 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import { CardActionArea } from "@mui/material";
 
 export default function PokemonCard({ name, image, types }) {
   //Função para retornar os tipos dos pokemons
   const typeHandle = () => {
     if (types[1]) {
-      return types[0].type.name + "" + types[1].type.name;
+      return types[0].type.name + " | " + types[1].type.name;
     }
     return types[0].type.name;
   };
@@ -23,12 +24,18 @@ export default function PokemonCard({ name, image, types }) {
           alt="green iguana"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {typeHandle()}
-          </Typography>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography gutterBottom variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography gutterBottom variant="caption" component="div">
+              {typeHandle()}
+            </Typography>
+          </Box>
           {/* <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
