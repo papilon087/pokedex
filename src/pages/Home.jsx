d1b5b22456cd44b68ba5c10267294865e0066945
papilon <ademirgus@gmail.com>
2023-06-28 20:41:28 -0300

    feat:navigate dados for page profile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 
 import Navbar from "../components/Navbar";
 import PokemonCard from "../components/PokemonCard";
 import { Skeletons } from "../components/Skeletons";
+import { useNavigate } from "react-router-dom";
 
-export const Home = () => {
+export const Home = ({ setPokemonData }) => {
   const [pokemons, setPokemons] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getPokemons();
@@ -40,6 +42,12 @@ export const Home = () => {
     setPokemons(filteredPokemons);
   };
 
+  //Função para pegar as informações do pokemon e jogar para a página profile.
+  const pokemonPickHandle = (pokemonData) => {
+    setPokemonData(pokemonData);
+    navigate("/profile");
+  };
+
   return (
     <div>
       <Navbar pokemonFilter={pokemonFilter} />
@@ -50,11 +58,13 @@ export const Home = () => {
           ) : (
             pokemons.map((pokemon, key) => (
               <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
-                <PokemonCard
-                  name={pokemon.data.name}
-                  image={pokemon.data.sprites.front_default}
-                  types={pokemon.data.types}
-                />
+                <Box onClick={() => pokemonPickHandle(pokemon.data)}>
+                  <PokemonCard
+                    name={pokemon.data.name}
+                    image={pokemon.data.sprites.front_default}
+                    types={pokemon.data.types}
+                  />
+                </Box>
               </Grid>
             ))
           )}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-export const Profile = () => {
+export const Profile = ({ pokemonData }) => {
+  console.log(pokemonData);
   return <Navbar hideSearch />;
 };
